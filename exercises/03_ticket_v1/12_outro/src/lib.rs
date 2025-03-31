9adbf720d8a79485d9e4348e6ca995694fa7f418
TurnedToast <149647574+TurnedToast@users.noreply.github.com>
2025-03-31 00:01:48 +0000

    finish beginning borrow exercises

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,45 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Self {
+        validate_product_name(&product_name);
+        validate_quantity(quantity);
+        validate_unit_price(unit_price);
+        Order {
+            product_name,
+            quantity: quantity,
+            unit_price: unit_price
+        }
+    }
+
+    pub fn product_name(&self) -> &String { &self.product_name }
+    pub fn quantity(&self) -> &i32 { &self.quantity }
+    pub fn unit_price(&self) -> &i32 { &self.unit_price }
+    pub fn set_product_name(&mut self, product_name: String) { self.product_name = product_name; }
+    pub fn set_quantity(&mut self, quantity: i32) { self.quantity = quantity; }
+    pub fn set_unit_price(&mut self, unit_price: i32) { self.unit_price = unit_price; }
+
+    pub fn total(&self) -> i32 {
+        self.quantity * &self.unit_price
+    }
+
+}
+
+fn validate_product_name(product_name: &String) {
+    if product_name.trim().len() == 0 { panic!("Product name cannot be empty"); }
+    if product_name.len() > 300 { panic!("Product name must be less than 300 characters!"); }
+}
+fn validate_quantity(quantity: i32) {
+    if quantity < 1 { panic!("Quantity must be greater than 0"); }
+}
+fn validate_unit_price(unit_price: i32) {
+    if unit_price < 1 { panic!("Unit price must be greater than 0"); }
+}
